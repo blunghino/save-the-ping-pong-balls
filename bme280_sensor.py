0f71bd9b6109c2e26f6483af89b85bdbd18717cf
Brent Lunghino <brentlunghino@gmail.com>
2020-10-07 04:20:39 +0100

    add port for bme280

--- bme280_sensor.py
+++ bme280_sensor.py
@@ -1,12 +1,13 @@
 import bme280
 import smbus2
 
-
-bme280_address = 0x77
-bme280_bus = smbus2.SMBus(port)
+bme280_port = 1
+bme280_address = 0x76
+bme280_bus = smbus2.SMBus(bme280_port)
 calibration_params = bme280.load_calibration_params(bme280_bus, bme280_address)
 
 
 def bme280_temperature() -> float:
     data = bme280.sample(bme280_bus, bme280_address, calibration_params)
     return data.temperature
+

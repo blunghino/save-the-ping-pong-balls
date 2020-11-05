ef457f800a571fb290735a38772f782d9db9a4bf
Brent Lunghino <brentlunghino@gmail.com>
2020-11-05 08:11:10 -0800

    Working query script

--- query_influx.py
+++ query_influx.py
@@ -1,10 +1,12 @@
 from datetime import date
 import os
+from typing import Optional
 
 from influxdb_client import InfluxDBClient
 import pandas as pd
 
 
+TZ = 'America/Los_Angeles'
 BUCKET = os.getenv('INFLUXDB_V2_BUCKET')
 
 
@@ -18,7 +20,7 @@ class InfluxQuery:
         days_back: int,
         experiment: str = 'seaside-kitchen-fridge-2',
     ) -> pd.DataFrame:
-        dfs = list()
+        dfs: List[pd.DataFrame] = list()
         for day_back in range(days_back):
             query_str = (
                 f'from(bucket: "{BUCKET}")'
@@ -31,26 +33,38 @@ class InfluxQuery:
                  '  |> aggregateWindow(every: 1s, fn: last, createEmpty: false)'
                  '  |> yield(name: "last")'
             )
-            dfs.append(self.query_api.query_data_frame(query_str))
+            df = self.query_api.query_data_frame(query_str)
+            dfs.extend(df)
         return pd.concat(dfs)
 
 
-def get_dataset(days_back: int, save: bool = True) -> pd.DataFrame:
-    today = date.today().isoformat()
+def get_dataset(
+    days_back: int,
+    save: bool = True,
+    end_date: Optional[str] = None
+) -> pd.DataFrame:
+    if end_date is None:
+        end_date = date.today().isoformat()
     file_path = os.path.join(
-        'data', 
-        f'sensorReadings-{days_back}-days-thru-{today}.csv'
-    )
+        'data', f'sensorReadings-{days_back}-days-thru-{end_date}.csv')
     try:
-        data = pd.read_csv(file_path)
+        print('Attempting to read from local file.')
+        data = pd.read_csv(file_path, parse_dates=True)
     except FileNotFoundError:
+        print('Querying InfluxDB.')
         query = InfluxQuery()
         data = query.get(days_back=days_back)
+        data = data.drop(
+            columns=['result', 'table', '_start', '_stop', '_measurement'])
+        data = data.iloc[:-1, :]
+        data.loc[:, '_time'] = pd.to_datetime(data.loc[:, '_time'])
+        data.loc[:, '_time'] = data.loc[:, '_time'].dt.tz_convert(TZ)
         if save:
-            data.to_csv(file_path)
+            data.to_csv(file_path, index=False)
+    data = data.set_index(['_field', '_time'], drop=False)
     return data
 
 if __name__ == '__main__':
-    data = get_dataset(days_back=2)
+    data = get_dataset(days_back=21, end_date='2020-11-04')
     print(data)
 
